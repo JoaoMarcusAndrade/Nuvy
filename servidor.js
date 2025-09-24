b26dfbc0a22dabb149eaec659cf04662476568bd
Okawss <160984216+Okawss@users.noreply.github.com>
2025-09-24 01:14:30 -0300

    esm

--- servidor.js
+++ servidor.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import routes from './routes.js';
 import cookieParser from 'cookie-parser';
+import { fileURLToPath } from 'url';
 
 const app = express();
 
