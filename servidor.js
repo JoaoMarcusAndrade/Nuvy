97b85e323e41a00033a69f0e6b8418511042cfb8
Okawss <160984216+Okawss@users.noreply.github.com>
2025-09-24 01:08:25 -0300

    audios nas rotas

--- servidor.js
+++ servidor.js
@@ -5,12 +5,20 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+
 app.use(cors({
   origin: 'https://nuvy-1.onrender.com',
   credentials: true
 }));
 app.use(cookieParser());
 app.use(express.json());
+
+// Arquivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
 const PORT = 3000;
